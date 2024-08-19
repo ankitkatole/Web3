5a0327b03af6d22990610be9c2fd52c5bd76eb15
ankitkatole <katoleankit06@gmail.com>
2024-08-19 16:50:27 +0530

    Week-2 Offline Completed

---
+++ Week-2/Offline/derivedPath.ts
@@ -0,0 +1,17 @@
+import nacl from "tweetnacl";
+import { generateMnemonic, mnemonicToSeedSync } from "bip39";
+import { derivePath } from "ed25519-hd-key";
+import { Keypair } from "@solana/web3.js";
+
+// const mnemonic = generateMnemonic();
+
+const mnemonic = 
+"destroy retire candy cotton asset field wheel gate auction borrow join predict";
+const seed = mnemonicToSeedSync(mnemonic);
+console.log(seed)
+for (let i = 0; i < 4; i++) {//i iterating till no. of accounts we have this mnemonic is example mnemonic with 4 accounts
+  const path = `m/44'/501'/${i}'/0'`; // This is the derivation path
+  const derivedSeed = derivePath(path, seed.toString("hex")).key;
+  const secret = nacl.sign.keyPair.fromSeed(derivedSeed).secretKey;
+  console.log(Keypair.fromSecretKey(secret).publicKey.toBase58());
+}
---
+++ Week-2/Offline/example.js
@@ -0,0 +1,4 @@
+let str = "A";
+const binaryRepresentation = new TextEncoder().encode(str); //returns Uint8Array
+console.log(binaryRepresentation);
+
